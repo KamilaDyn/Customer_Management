a007df65bb5f44eb28a1e21c9895d4fca9e16274
Kamila Dynysiuk <kamila0990@gmail.com>
2019-06-13 00:30:44 +0100

    make it better

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const items = require("./routes/api/items");
 
 const app = express();
+const path = require("path");
 var cors = require("cors");
 
 app.use(cors());
@@ -25,6 +26,12 @@ mongoose
   .catch(error => console.log(error));
 
 app.use("/api/items", items);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
